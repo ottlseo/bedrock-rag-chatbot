f4886b5625db5ee9ea904a48f6538e8f6635e825
Yoonseo Kim <61778930+ottlseo@users.noreply.github.com>
2025-02-09 23:18:39 +0900

    Feat: add variables.py file

--- application/bedrock.py
+++ application/bedrock.py
@@ -1,20 +1,7 @@
 import os, sys, boto3
+from variables import KNOWLEDGE_BASE_ID, MODEL_ARN, REGION
 
-region_name = boto3.Session().region_name
-ssm = boto3.client('ssm', region_name=region_name)
-bedrock_agent_runtime_client = boto3.client("bedrock-agent-runtime", region_name="us-west-2")
-
-def get_knowledge_base_id(key, enc=False):
-    if enc: WithDecryption = True
-    else: WithDecryption = False
-    response = ssm.get_parameters(
-        Names=[key,],
-        WithDecryption=WithDecryption
-    )
-    return response['Parameters'][0]['Value']
-
-MODEL_ARN = "anthropic.claude-3-sonnet-20240229-v1:0"
-KNOWLEDGE_BASE_ID = get_knowledge_base_id(key="/RAGChatBot/KNOWLEDGE_BASE_ID", enc=False)
+bedrock_agent_runtime_client = boto3.client("bedrock-agent-runtime", region_name=REGION)
 
 def query(question):
     response = bedrock_agent_runtime_client.retrieve_and_generate(
---
+++ application/variables.py
@@ -0,0 +1,4 @@
+KNOWLEDGE_BASE_ID="<YOUR_KNOWLEDGE_BASE_ID>" # Please update
+
+REGION="us-west-2"
+MODEL_ARN="anthropic.claude-3-sonnet-20240229-v1:0"
